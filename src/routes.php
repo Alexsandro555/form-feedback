63186805feb8abe1d2dd74c589f18a1251560893
alexsandro <alexsandro@oooleader.ru>
2018-04-05 17:23:51 +0300

    fdsfd

--- src/routes.php
+++ src/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['web'], 'namespace' =>'Leader\FormFeedback\Controllers'], function () {
+Route::group(['middleware' => ['web']], function () {
   ////////////////////////////////Form-feedback///////////////////////////////////////////////////////
   Route::get('/getUID', ['uses' => 'Leader\FormFeedback\Controllers\FormFeedbackController@getUID', 'as' => 'getUID']);
   Route::post('/form-feedback/save', [
